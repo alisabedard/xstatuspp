a3d6ea1d8336f852dfd61a7f1cbe6ea8f8bd1e00
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-25 22:49:24 -0500

    Use iostream output of usage text.

--- main.cpp
+++ main.cpp
@@ -1,28 +1,23 @@
 // Copyright 2016, Jeffrey E. Bedard
-#include <cstdio>
-#include <cstdlib>
+#include <iostream>
 #include <unistd.h>
 #include "config.h"
 #include "xstatus.h"
-#define XSTATUS_HELPTEXT \
-	"DESCRIPTION:  Simple X toolbar for minimalistic"\
-	" window managers.\n" \
-	"USAGE: xstatus [-d DELAY][-f FILE][-h]\n" \
-	"\t-d DELAY\tSet delay between status updates," \
-	" in seconds.\n" \
-	"\t-f FILE\t\tSet FILE to be continuously polled and" \
-	" displayed.\n" \
-	"\t-h\t\tPrint this usage information.\n" \
-	"Copyright 2016, Jeffrey E. Bedard <jefbed@gmail.com>\n" \
-	"Project page:  https://github.com/jefbed/xstatus\n"
-__attribute__((noreturn))
+using namespace std;
 static void usage(void)
 {
-	fputs(XSTATUS_HELPTEXT, stderr);
+	cout << "DESCRIPTION: Simple X toolbar for minimalistic "
+		"window managers.\n"
+		"USAGE: xstatus [-d DELAY][-f FILE][-h]\n"
+		"\t-d DELAY\tSet delay between status updates, in seconds.\n"
+		"\t-f FILE\t\tSet FILE to be continuously polled "
+		"and displayed.\n"
+		"\t-h\t\tPrint this usage information.\n"
+		"Copyright 2016, Jeffrey E. Bedard <jefbed@gmail.com>\n"
+		"Project page: https://github.com/jefbed/xstatus\n";
 	exit(1);
 }
-static void parse_command_line(int argc, char ** argv,
-	struct XStatusOptions *  o)
+static void parse_command_line(int argc, char ** argv, XStatusOptions * o)
 {
 	int8_t opt;
 	while((opt = getopt(argc, argv, "d:f:h")) != -1) {
@@ -42,7 +37,7 @@ static void parse_command_line(int argc, char ** argv,
 int main(int argc, char ** argv)
 {
 	char default_filename[] = XSTATUS_STATUS_FILE;
-	struct XStatusOptions o = {.filename = default_filename,
+	XStatusOptions o = {.filename = default_filename,
 		.delay = 1};
 	parse_command_line(argc, argv, &o);
 	xstatus::start(&o);
