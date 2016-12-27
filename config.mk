00a795d1aba4ed00f390557ed3da8387dca7bdd8
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-27 00:26:10 -0500

    Removed autogenerated file.

--- config.mk
+++
@@ -1,8 +0,0 @@
-CFLAGS=
-CFLAGS+=-DLINUX
-CFLAGS+=-D_GNU_SOURCE
-CXXFLAGS=-O0 -W -Wall -Werror -march=sandybridge -pipe -ggdb
-clang=search
-clang=search
-CFLAGS+=-O0 -ggdb -Werror -DDEBUG
-static=libjb/libjb.a
