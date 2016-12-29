d360bf8af12d3726f2f409912bc5827b97debbde
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-29 03:48:16 -0500

    Combined expression.

--- Buffer.cpp
+++ Buffer.cpp
@@ -2,7 +2,6 @@
 #include "Buffer.h"
 xstatus::Buffer::Buffer(const Buffer &obj) // copy
 {
-	size = obj.size;
-	buffer = new char [size];
+	buffer = new char [size = obj.size];
 	((std::string)obj).copy(buffer, size);
 }
