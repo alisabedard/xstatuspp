3418dd409d37ac09e2ad546e8aeb3566ac100ded
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-30 02:05:09 -0500

    Used new debug macros in libjb/cpp.h.

--- Options.cpp
+++ Options.cpp
@@ -1,30 +1,33 @@
 // Copyright 2017, Jeffrey E. Bedard <jefbed@gmail.com>
 #include "Options.h"
 #include <string>
+#include "libjb/cpp.h"
 #include "libjb/log.h"
 using namespace std;
 using namespace xstatus;
+int Options::instances = 0;
 Options::Options(char * filename)
 {
-	LOG("Options constructor");
+	JB_LOG_ADD(Options, instances);
 	string s(filename);
 	this->filename = new char[s.size()];
 	s.copy(this->filename, s.size());
 }
 Options::Options(const Options& obj)
 {
-	LOG("Options copy constructor");
+	JB_LOG_ADD(Options, instances);
 	string s(obj.filename);
 	this->filename = new char[s.size()];
 	s.copy(this->filename, s.size());
 }
 Options::~Options(void)
 {
-	LOG("Options destructor");
+	JB_LOG_DEL(Options, instances);
 	delete filename;
 }
 void Options::set_filename(char * filename)
 {
+	JB_LOG_OP(Options, "set_filename()");
 	delete this->filename;
 	string s(filename);
 	this->filename = new char[s.size()];
