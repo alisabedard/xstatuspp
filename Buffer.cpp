ba2f4a3dc4cab74c8b78958639b333737f422e70
Jeffrey E. Bedard <jefbed@gmail.com>
2016-12-30 01:45:27 -0500

    Removed trivial debug code.

--- Buffer.cpp
+++ Buffer.cpp
@@ -1,30 +1,23 @@
 // Copyright 2017, Jeffrey E. Bedard <jefbed@gmail.com>
 #include "Buffer.h"
-#include "libjb/log.h"
+#include <iostream>
 #include "libjb/macros.h"
+#undef DEBUG
 using namespace xstatus;
 int Buffer::instances = 0;
-#define DEBUG_BUFFER
-#ifdef DEBUG_BUFFER
-#define log_init(msg, op) LOG("%s Buffer %d", #msg, op instances);
-#else//!DEBUG_BUFFER
-#define log_init(msg, op)
-#endif//DEBUG_BUFFER
-Buffer::Buffer(size_t sz) : size(sz), max_size(sz), buffer(new char [sz])
+Buffer::Buffer(size_t sz) : size(sz), max_size(sz),
+	buffer(new char [sz])
 {
-	log_init(new, ++);
 }
 Buffer::Buffer(const Buffer &obj) // copy
 	// make sure it can hold max_size
 	: max_size(obj.max_size), size(obj.size),
 	buffer(new char[obj.max_size])
 {
-	log_init(copy, ++);
 	std::string(obj).copy(buffer, size);
 }
 Buffer::~Buffer(void)
 {
-	log_init(delete, --);
 	delete[] buffer;
 }
 void Buffer::set_size(const size_t s)
